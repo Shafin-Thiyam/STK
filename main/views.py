c76744cbb6c123a77d3eb05842beb3ea586f9bb5
shafin thiyam <shafin.thiyam@outlook.com>
2019-09-09 21:13:06 +0530

    mail update

--- main/views.py
+++ main/views.py
@@ -14,8 +14,7 @@ def index(request):
         msg= request.POST['message']
         contacted=contact(Name=recruiterName, email=recruiterEmail,subject=msgSub,message=msg)
         contacted.save()
-        send_mail('Re:'+msgSub,'<p>Thanks '+recruiterName+', for contacting me. Looking forward to discuss further about the position if my profile suite your requirement</p>', 'shafin.tk@outlook.com',[recruiterEmail,'shafin.thiyam@outlook.com'], fail_silently=False)
-        messages.success(request,'We contact as soon as possible')
+        send_mail('Re:'+msgSub,'Thanks '+recruiterName+', for contacting me.'+"\n"+'Looking forward to discuss further about the position if my profile suite your requirement', 'thiyam.shafin@gmail.com',[recruiterEmail,'shafin.thiyam@outlook.com'], fail_silently=False)
         messages.success(request,'Really Thankfull for contacting me')
         return redirect('/')
     else:
--- shafinthiyam/settings.py
+++ shafinthiyam/settings.py
@@ -143,7 +143,7 @@ MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 #email config
-EMAIL_HOST = 'smtp.live.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PWD')
