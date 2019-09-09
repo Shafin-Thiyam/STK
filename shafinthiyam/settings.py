25ba9ada11bb4355ad44444cb9e879d6fb131619
shafin thiyam <shafin.thiyam@outlook.com>
2019-09-09 18:53:19 +0530

    add allowed host

--- shafinthiyam/settings.py
+++ shafinthiyam/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = ['murmuring-beach-71202.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['murmuring-beach-71202.herokuapp.com','shafinthiyam.herokuapp.com','localhost']
 
 
 # Application definition
