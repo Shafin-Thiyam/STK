c6c1ba258033b564a2ac44a0c0a9c275b0ba884e
shafin thiyam <shafin.thiyam@outlook.com>
2019-09-09 16:24:33 +0530

    add allowed host

--- shafinthiyam/settings.py
+++ shafinthiyam/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '$31@141m40a(33et2lfj+*24=z-ue4mj*^papgif9)!gt-bbk2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['murmuring-beach-71202.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['murmuring-beach-71202.herokuapp.com']
 
 
 # Application definition
