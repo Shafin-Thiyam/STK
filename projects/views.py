f05c43ca23339b9f1ddf4cb28e2dd798ef3ac59f
shafin thiyam <shafin.thiyam@outlook.com>
2019-10-09 21:06:41 +0530

    update orm for experience in project

--- projects/views.py
+++ projects/views.py
@@ -7,7 +7,8 @@ def project(request, Employee_id):
     PrjDetails=Projects.objects.filter(Employee_id=Employee_id)
     # Recent_Desg=Experience.objects.all().order_by('-From')[0:1]
     Recent_Desg=(Experience.objects.all().filter(visible=True).order_by('-From')[0:1])[0].Designation
-    ExperienceDetails=Experience.objects.all().order_by('-From')
+    # ExperienceDetails=Experience.objects.all().order_by('-From')
+    ExperienceDetails=Experience.objects.all().filter(visible=True).order_by('-From')
     eid=Projects.objects.filter(Employee_id=Employee_id)[0:1]
     Personal=PersonalDetails.objects.all()
     #PrjDetails=Projects.objects.all()
